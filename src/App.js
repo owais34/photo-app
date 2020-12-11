a50dc7175477c46a91de6a80b4108412b4004db0
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2020-12-11 21:37:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { Button } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Gallery from './components/Gallery/Gallery';
+import Header from './components/Header/Header';
+import {url} from '../src/apidata'
 
 function App() {
+  const [button_active,toggle_button]=useState(false);
+  const [countDown,setCountDown]=useState(-1)
+  const [tileData,setTileData]=useState([])
+
+  useEffect(()=>{
+    if(countDown!==-1)
+    {
+      setTimeout(()=>{setCountDown(countDown-1)},1000)
+    }
+    else
+    {
+      fetch(`${url}&&count=${4}`)
+    .then(res=>res.json())
+    .then(data=>{setTileData(data)})
+    .catch(err=>console.log(err))
+      toggle_button(false)
+    }
+  },[countDown])
+
+  useEffect(()=>{
+    fetch(`${url}&&count=${4}`)
+    .then(res=>res.json())
+    .then(data=>{setTileData(data)})
+    .catch(err=>console.log(err))
+
+  },[])
+
+  function button_click_event(){
+    toggle_button(true)
+    setCountDown(10)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Gallery tileData={tileData}/>
+      <Button color="primary" variant="contained" disabled={button_active}
+      onClick={()=>button_click_event()}>Reload New Images</Button>
+      {(countDown!==-1)?(<h2>{countDown}</h2>):null}
     </div>
   );
 }
